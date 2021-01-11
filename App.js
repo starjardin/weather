5535252aa6ae684fee4b8184b1ceda5f6267b7be
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-11 15:29:07 +0300

    added app.js

--- App.js
+++ App.js
@@ -1,79 +1,65 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import Locations from './components/locations'
+import Main from './components/Main'
 
+const  CORS_KEY = "https://cors-anywhere.herokuapp.com/"
+const API_KEY = "https://www.metaweather.com/api/"
+const API_LOCATION = "/api/location/search/?query="
+"https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/2487956"
 export default function App () {
 
   const [ location, setLocation ] = useState('london')
   const [ weatherLocationObj, setWeatherLocationObj ] = useState([])
-  const [ weatherInFiveDays, setWeatherInFiveDays ] = useState([])
-
-  const  CORS_KEY = "https://cors-anywhere.herokuapp.com/"
-  const API_KEY = "https://www.metaweather.com/api/"
-  const API_LOCATION = "/api/location/search/?query="
+  const [weatherInFiveDays, setWeatherInFiveDays] = useState([])
+  const [woeid, setWoeid] = useState('')
 
   async function fetchData() {
-    const res= await axios.get(CORS_KEY + API_KEY + API_LOCATION + location)
-    console.log(res.data);
+    const res = await axios.get(CORS_KEY + API_KEY + API_LOCATION + location)
     setWeatherLocationObj(res.data)
   }
 
-  useEffect (() => {
-    fetchData()
-  }, [])
-
-  useEffect (() => {
-    getWeather()
-  }, [location])
-
-  let date = new Date()
-  const localDate = date.toLocaleDateString();
-  const splitedDate = localDate.split("/");
-  const americanDate = `${splitedDate[2]}/${splitedDate[0]}/${splitedDate[1]}`
-
-  let woeid = weatherLocationObj.length > 0 && weatherLocationObj[0].woeid
-  
   const getWeather = async () => {
-    const data = await axios.get(CORS_KEY + API_KEY + "location/" + `${woeid ? woeid : ""}`)
-    setWeatherInFiveDays(data)
+    const res = await axios.get(CORS_KEY + API_KEY + "location/" + woeid)
+    setWeatherInFiveDays(res.data)
   }
 
+  useEffect(() => {
+    if (location != '') {
+      fetchData()
+    } else {
+      return
+    }
+  }, [location])
+
+  useEffect(() => {
+    getWeather()
+  }, [woeid])
+  
   const handleSearch = (e) => {
     e.preventDefault()
     const searchLocation = e.target.search.value
-    if (searchLocation.trim() == "") {
-      console.log("Hello world");
+    if (searchLocation.trim() === "") {
+      console.log("Hello world")
     }
     setLocation(searchLocation)
   }
 
-  function handleChange (e) {
-    const location = e.target.value
-    setLocation(location)
+  function handleClick(e) {
+    const locationValue = e.target.dataset.value
+    const selectedLocation = weatherLocationObj.filter(location => location.title === locationValue)
+    setWoeid(selectedLocation[0].woeid);
   }
+  console.log(woeid)
 
-  function handleClick (locationFound) {
-    setLocation(locationFound)
-  }
-
-  if (!weatherLocationObj.length) return null
   return (
     <>
-      <h1>Hello world</h1>
-      <h2>{}</h2>
-      <form action="#" className="form-search" onSubmit={handleSearch}>
-        <label htmlFor="search-input">Search Location</label>
-        <input 
-          name="search"
-          id="search-input"
-        />
-        <button type="submit" >search</button>
-        <Locations
-          weatherLocationObj={weatherLocationObj}
-          handleClick={handleClick}
-        />
-      </form>
+      <Main
+        weatherLocationObj={weatherLocationObj}
+        handleClick={handleClick}
+        handleSearch={handleSearch}
+        weatherInFiveDays={weatherInFiveDays}
+      />
     </>
   )
 }
