7eb66cbfdd1a13f143f9578a955fd314d5337881
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-11 10:37:45 +0300

    fixing and refactoring functions

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import Locations from './components/locations'
 
 export default function App () {
 
-  const [ location, setLocation ] = useState('san')
+  const [ location, setLocation ] = useState('london')
   const [ weatherLocationObj, setWeatherLocationObj ] = useState([])
   const [ weatherInFiveDays, setWeatherInFiveDays ] = useState([])
 
@@ -10,20 +13,31 @@ export default function App () {
   const API_KEY = "https://www.metaweather.com/api/"
   const API_LOCATION = "/api/location/search/?query="
 
-  async function fetchData () {
-    const res= await fetch(CORS_KEY + API_KEY + API_LOCATION + location)
-    const data = await res.json()
-    setWeatherLocationObj(data)
+  async function fetchData() {
+    const res= await axios.get(CORS_KEY + API_KEY + API_LOCATION + location)
+    console.log(res.data);
+    setWeatherLocationObj(res.data)
   }
 
-  console.log(weatherLocationObj);
+  useEffect (() => {
+    fetchData()
+  }, [])
 
+  useEffect (() => {
+    getWeather()
+  }, [location])
+
+  let date = new Date()
+  const localDate = date.toLocaleDateString();
+  const splitedDate = localDate.split("/");
+  const americanDate = `${splitedDate[2]}/${splitedDate[0]}/${splitedDate[1]}`
+
+  let woeid = weatherLocationObj.length > 0 && weatherLocationObj[0].woeid
+  
   const getWeather = async () => {
-    const res = await fetch(CORS_KEY + API_KEY + API_LOCATION + weatherLocationObj.woeid)
-    const data = await res.json()
+    const data = await axios.get(CORS_KEY + API_KEY + "location/" + `${woeid ? woeid : ""}`)
     setWeatherInFiveDays(data)
   }
-  console.log(weatherInFiveDays);
 
   const handleSearch = (e) => {
     e.preventDefault()
@@ -34,11 +48,16 @@ export default function App () {
     setLocation(searchLocation)
   }
 
-  useEffect (() => {
-    fetchData()
-    getWeather()
-  }, [ location ])
+  function handleChange (e) {
+    const location = e.target.value
+    setLocation(location)
+  }
 
+  function handleClick (locationFound) {
+    setLocation(locationFound)
+  }
+
+  if (!weatherLocationObj.length) return null
   return (
     <>
       <h1>Hello world</h1>
@@ -50,6 +69,10 @@ export default function App () {
           id="search-input"
         />
         <button type="submit" >search</button>
+        <Locations
+          weatherLocationObj={weatherLocationObj}
+          handleClick={handleClick}
+        />
       </form>
     </>
   )
