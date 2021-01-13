e342b38d244d500c283babe228c0c1702f45ffe2
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-13 15:29:08 +0300

    added weather styles

--- components/Weather.js
+++ components/Weather.js
@@ -1,61 +1,30 @@
-import React, { useContext } from 'react'
-import styled from 'styled-components'
-import { GlobalContext } from '../useReducer'
-
-const WeatherInFiveDaysStyles = styled.div`
-  display : flex;
-  gap : 1rem;
-  justify-content : space-between;
-
-  div {
-    background-color : #1E213A;
-    padding : 1rem;
-  }
-
-  .max-min-temp {
-    display : flex;
-    justify-content : space-between;
-  }
-`
-const HilightsStyles = styled.div`
-  margin : 1rem;
-  div {
-    background-color : #1E213A;
-    padding : 1rem;
-    margin : 1rem
-  }
-  @media screen and (min-width : 720px) {
-    display : grid;
-    grid-template-columns : repeat(2, 1fr);
-    gap : 2rem;
-    div {
-      margin : 0;
-    }
-  }
-`
+import React, { useContext, useState } from 'react'
+import { GlobalContext, API_KEY } from '../useReducer'
+import { FaLocationArrow } from 'react-icons/fa'
+import { WeatherInFiveDaysStyles, HilightsStyles } from './WeatherInFiveDaysStyles'
 
 export default function Weather() {
 
   const { state } = useContext(GlobalContext)
+  let futureWeather = state.weatherInFiveDays.consolidated_weather
+  const [futureWeathe, setFutureWeather] = useState({})
+  console.log(futureWeathe);
   
-  const futureWeather = state.weatherInFiveDays.consolidated_weather
   const weatherFromTomorow = futureWeather?.splice(1)
   console.log(weatherFromTomorow);
+  console.log(futureWeather);
+  const weatherHighlits = (itemId) => {
+    // setFutureWeather(weatherFromTomorow?.filter(weather => weather.id === itemId))
+  }
 
   const futureWeatherElem = (weatherFromTomorow) => {
     return weatherFromTomorow
       ? <WeatherInFiveDaysStyles> {weatherFromTomorow.map((weather, index) =>
-        <div key={index}>
+        <div key={index} onClick={() => weatherHighlits(weather.id)}>
           <p>
             {index === 0 ? "Tomorrow" : weather.created}
           </p>
-          <p>
-            {weather.weather_state_name}
-          </p>
-          <picture>
-            <source srcSet = {`/static/img/weather/${weather.weather_state_abbr}.svg`} alt={`${weather.weather_state_name}`}></source>
-          </picture>
-          <img src={`/static/img/weather/${weather.weather_state_abbr}.svg`} alt={`${weather.weather_state_name}`} />
+          <img src={`https://www.metaweather.com//static/img/weather/${weather.weather_state_abbr}.svg`} alt={`${weather.weather_state_name}`} />
           <div className="max-min-temp">
             <span> {Math.round(weather.max_temp)}</span>
             <span> {Math.round(weather.min_temp)}</span>
@@ -69,17 +38,18 @@ export default function Weather() {
 
   return (
     <div>
-      <h2>Hello world</h2>
       {futureWeatherElem(weatherFromTomorow)}
       {futureWeather && <div className="heilight">
         <h3>
-          Hilight for today
+          Today's Highlight
         </h3>
         <HilightsStyles className="hilight-container">
           <div>
             <h4>wind status</h4>
             <div>{futureWeather && Number(futureWeather[0].wind_direction).toFixed(2)} mph</div>
-            <div>{futureWeather && futureWeather[0].wind_direction_compass}</div>
+            <div>
+              <FaLocationArrow className="arrow-location"/>  {futureWeather && futureWeather[0].wind_direction_compass}
+            </div>
           </div>
           <div>
             <h4>Humidity</h4>
