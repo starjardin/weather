bdc73ee55270da0c336ec73920e2ee6da908ac11
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-13 09:35:17 +0300

    added min and max temperature

--- components/Weather.js
+++ components/Weather.js
@@ -6,13 +6,23 @@ const WeatherInFiveDaysStyles = styled.div`
   display : flex;
   gap : 1rem;
   justify-content : space-between;
+
+  div {
+    background-color : #1E213A;
+    padding : 1rem;
+  }
+
+  .max-min-temp {
+    display : flex;
+    justify-content : space-between;
+  }
 `
 const HilightsStyles = styled.div`
   margin : 1rem;
   div {
     background-color : #1E213A;
-    padding : 2rem;
-    margin : 2rem;
+    padding : 1rem;
+    margin : 1rem
   }
   @media screen and (min-width : 720px) {
     display : grid;
@@ -30,18 +40,26 @@ export default function Weather() {
   
   const futureWeather = state.weatherInFiveDays.consolidated_weather
   const weatherFromTomorow = futureWeather?.splice(1)
-  
+  console.log(weatherFromTomorow);
+
   const futureWeatherElem = (weatherFromTomorow) => {
     return weatherFromTomorow
       ? <WeatherInFiveDaysStyles> {weatherFromTomorow.map((weather, index) =>
         <div key={index}>
           <p>
-          {weather.weather_state_name}
+            {index === 0 ? "Tomorrow" : weather.created}
+          </p>
+          <p>
+            {weather.weather_state_name}
           </p>
           <picture>
             <source srcSet = {`/static/img/weather/${weather.weather_state_abbr}.svg`} alt={`${weather.weather_state_name}`}></source>
           </picture>
-          <img src={`/static/img/weather/${weather.weather_state_abbr}.svg`} alt={`${weather.weather_state_name}`}/>
+          <img src={`/static/img/weather/${weather.weather_state_abbr}.svg`} alt={`${weather.weather_state_name}`} />
+          <div className="max-min-temp">
+            <span> {Math.round(weather.max_temp)}</span>
+            <span> {Math.round(weather.min_temp)}</span>
+          </div>
         </div>
       )
       }
@@ -60,15 +78,17 @@ export default function Weather() {
         <HilightsStyles className="hilight-container">
           <div>
             <h4>wind status</h4>
+            <div>{futureWeather && Number(futureWeather[0].wind_direction).toFixed(2)} mph</div>
             <div>{futureWeather && futureWeather[0].wind_direction_compass}</div>
           </div>
           <div>
             <h4>Humidity</h4>
+            <p>{ futureWeather && futureWeather[0].humidity } % </p>
             <progress max="100" value={futureWeather && futureWeather[0].humidity}></progress>
           </div>
           <div>
             <h4>Visibility</h4>
-            <div>{futureWeather && futureWeather[0].visibility} miles</div>
+            <div>{futureWeather && Number(futureWeather[0].visibility).toFixed(2)} miles</div>
           </div>
           <div>
             <h4>Air Presure</h4>
