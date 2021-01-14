1d9cb6324ad254909e4caa08ac81ef044726ec01
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-14 14:31:08 +0300

    fixing the style of max-temp and min-temp

--- components/WeatherInFiveDaysStyles.js
+++ components/WeatherInFiveDaysStyles.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 export const WeatherInFiveDaysStyles = styled.div`
   display : flex;
   gap : 1rem;
-  // justify-content : space-between;
   flex-wrap : wrap;
   text-align : center;
+  max-width : 650px;
+  margin : auto;
 
   div {
     background-color : #1E213A;
@@ -14,23 +15,34 @@ export const WeatherInFiveDaysStyles = styled.div`
     max-width : 120px;
   }
 
-  .max-min-temp {
-    display : flex;
-    justify-content : space-between;
-  }
-
   img  {
     max-width : 56px;
     max-height : 62px;
   }
+
+  .max-temp {
+    text-align : left;
+  }
+
+  .min-temp {
+    text-align : right;
+  }
+
+  @media screen and (min-width : 720px) {
+    justify-content : space-between;
+  }
 `
 export const HilightsStyles = styled.div`
   margin : 1rem;
+  max-width : 650px;
+  margin : auto;
+
   div {
     background-color : #1E213A;
     padding : 1rem;
     margin : 1rem;
     text-align : center;
+    max-width : 328px;
   }
 
   @media screen and (min-width : 720px) {
