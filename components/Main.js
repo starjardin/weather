91e112f5e79e69642bf2da796cd36e9d07343490
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-14 14:28:22 +0300

    added search field and weather feild in the left panel

--- components/Main.js
+++ components/Main.js
@@ -1,25 +1,67 @@
-import React from 'react'
-import MainStyles from './MainStyles'
+import React, { useState, useContext } from 'react'
+import { VscLocation } from 'react-icons/vsc'
 
+import MainStyles from './MainStyles'
 import Locations from './locations'
 import Weather from './Weather'
+import { GlobalContext } from '../useReducer'
 
 export default function Main({ handleSearch }) {
+  const { state } = useContext(GlobalContext)
+  const [searchIsOpen, setSearchIsOpen] = useState(false)
+   
+  const todaysWeather = state.weatherInFiveDays.consolidated_weather?.[0]
+  const todaysDate = todaysWeather?.created
+  const date = new Date(todaysDate)
+  const temperature = Number(todaysWeather?.the_temp).toFixed(2)
+  
+  function openSearch() {
+    setSearchIsOpen(true)
+  }
+
+  function closeSearch() {
+    setSearchIsOpen(false)
+  }
+
   return (
     <MainStyles>
       <div className="search-feild">
-        <h1>Hello world</h1>
-        <form action="#" className="form-search" onSubmit={handleSearch}>
-          <label htmlFor="search-input">Search Location</label>
-          <input 
-            name="search"
-            id="search-input"
-            autoComplete="off"
-            placeholder = "london"
-          />
-          <button type="submit" className="search">search</button>
-          <Locations />
-        </form>
+        {searchIsOpen
+          ? <div>
+              <form action="#" className="form-search" onSubmit={handleSearch}>
+                <label htmlFor="search-input">Search Location</label>
+                <input 
+                  name="search"
+                  id="search-input"
+                  autoComplete="off"
+                  placeholder="london"
+                />
+              <button type="button" onClick={closeSearch}>&#10006;</button>
+              <button type="submit" className="search">search</button>
+            </form>
+            <Locations
+              setSearchIsOpen={setSearchIsOpen}
+            />
+          </div>
+          :
+          <div className="todays-weather">
+            <input onClick={openSearch} />
+            <div className="weather-today">
+              {todaysWeather
+                ? <div>
+                  <img src={`https://www.metaweather.com//static/img/weather/${todaysWeather.weather_state_abbr}.svg`} alt={`${todaysWeather.weather_state_name}`} />
+                  <p> <span className="heat">{Number(temperature)}</span> C</p>
+                  <p>{todaysWeather.weather_state_name}</p>
+                  <p>Today : {todaysDate}</p>
+                  <p style={{margin : 0}}>
+                    <VscLocation /> {state.weatherInFiveDays?.title}
+                  </p>
+                  </div>
+                : <h4>Loading...</h4>
+              }
+            </div>
+          </div>
+        }
       </div>
       <div className="weather">
         <Weather/>
